29947b228980b58032d01fdeffdbd4271de7110e
makslazovsky <makslazovsky@ya.ru>
2022-05-26 01:48:00 +0300

    test

--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../main-project/data_subscription.h"
+#include "../main-project/processing.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
